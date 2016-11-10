e9f822b0cd11188e4d77d4b9539233b29ddc4cef
HEmam <HEmam@LT71>
2016-11-10 14:52:35 +0000

    Added Resources and Resources Assignment Projects; Still in progress

---
+++ ResourceAssignment/ResourceAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+using Resources;
+
+namespace ResourceAssignments
+{
+    public class ResourceAssignment
+    {
+        private IActivity _activity;
+        private Resource _resource;
+        private float _budgetedUnits;
+        private float 
+    }
+}
---
+++ Resources/Resource.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources
+{
+    public class Resource
+    {
+        public enum resourceType
+        {
+            Labour=1,
+            Material=2,
+            Equipment=3,
+            Money=4
+        };
+        private string resID;
+        private string resName;
+        private resourceType resType;
+        private float defUnitTime;
+        private float _unitCost;
+
+        public Resource()
+        {
+
+        }
+        public Resource( string ID, string Name, resourceType type, float defaultUnits, float unitCost)
+        {
+            this.ResID = ID;
+            this.ResName = Name;
+            this.ResType = type;
+            this.DefUnitTime = defaultUnits;
+            this.UnitCost = unitCost;
+        }
+        public string ResID
+        {
+            get
+            {
+                return resID;
+            }
+
+            set
+            {
+                resID = value;
+            }
+        }
+
+        public string ResName
+        {
+            get
+            {
+                return resName;
+            }
+
+            set
+            {
+                resName = value;
+            }
+        }
+
+        public resourceType ResType
+        {
+            get
+            {
+                return resType;
+            }
+
+            set
+            {
+                resType = value;
+            }
+        }
+
+        public float DefUnitTime
+        {
+            get
+            {
+                return defUnitTime;
+            }
+
+            set
+            {
+                defUnitTime = value;
+            }
+        }
+
+        public float UnitCost
+        {
+            get
+            {
+                return _unitCost;
+            }
+
+            set
+            {
+                _unitCost = value;
+            }
+        }
+    }
+}
