390954f95a9a51690895cd98fe80a9b655268c31
HEmam <HEmam@LT71>
2016-11-10 15:35:54 +0000

    Added Helper Utilities to calculate dates based on working days

--- Calendars/Calendar.cs
+++ Calendars/Calendar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calendars;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace Calendars
 {
-    public class Calendar
+    public static class Calendar
     {
-
+        
     }
 }
+
---
+++ Utilities/HelperFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class HelperFunctions
+    {
+        public static DateTime AddWorkdays(this DateTime originalDate, int workDays)
+        {
+            DateTime tmpDate = originalDate;
+            while (workDays > 0)
+            {
+                tmpDate = tmpDate.AddDays(1);
+                if (tmpDate.DayOfWeek < DayOfWeek.Saturday &&
+                    tmpDate.DayOfWeek > DayOfWeek.Sunday &&
+                    !tmpDate.IsHoliday())
+                    workDays--;
+            }
+            return tmpDate;
+        }
+
+        public static bool IsHoliday(this DateTime originalDate)
+        {
+            // INSERT YOUR HOlIDAY-CODE HERE!
+            return false;
+        }
+    }
+}
